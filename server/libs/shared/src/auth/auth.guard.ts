75d93a93cf37a15c804c54c679a99436cdd56900
Leadgq <2643336540@qq.com>
2026-01-30 19:47:24 +0800

    fix(auth): 使用UnauthorizedException替换Error抛出异常
    将auth.guard中的错误抛出从Error改为更合适的UnauthorizedException

--- server/libs/shared/src/auth/auth.guard.ts
+++ server/libs/shared/src/auth/auth.guard.ts
@@ -1,31 +1,34 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { JwtService } from '@nestjs/jwt';
 import { RefreshToken } from '@en/common/user';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(
-    private readonly jwtService: JwtService
-  ) {}
+  constructor(private readonly jwtService: JwtService) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
     const headers = request.headers;
     if (!headers.authorization) {
-      throw new Error('别偷了哥');
+      throw new UnauthorizedException('别偷了哥,买点东西吧');
     }
     const token = headers.authorization.split(' ')[1];
     try {
       const payload = this.jwtService.verify<RefreshToken>(token);
       if (payload.tokenType !== 'refresh') {
-        throw new Error('token类型错误');
+        throw new UnauthorizedException('token类型错误');
       }
       request.user = payload;
       return true;
     } catch (err) {
-      throw new Error('token 失效了');
+      throw new UnauthorizedException('token 失效了');
     }
   }
 }
