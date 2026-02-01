5740b44de1a0c2fe6fd2622a979d83f61887a026
Leadgq <2643336540@qq.com>
2026-02-01 09:39:50 +0800

    refactor(word-book): 移除单词本控制器的身份验证守卫
    该接口不再需要身份验证，因此移除AuthGuard装饰器以简化访问

--- server/apps/server/src/word-book/word-book.controller.ts
+++ server/apps/server/src/word-book/word-book.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { WordBookService } from './word-book.service';
 import type { WordQuery } from '@en/common/word';
-import { AuthGuard } from '@libs/shared/auth/auth.guard';
 
 @Controller('word-book')
 export class WordBookController {
   constructor(private readonly wordBookService: WordBookService) {}
 
-  @UseGuards(AuthGuard)
   @Get()
   findAll(@Query() query) {
     return this.wordBookService.findAll(query);
