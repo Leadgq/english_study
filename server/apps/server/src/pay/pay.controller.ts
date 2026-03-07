6535f9fd09f9df59d29aca097efb677428440330
Leadgq <2643336540@qq.com>
2026-03-07 16:51:12 +0800

    feat: introduce payment creation and notification handling with new service and controller.

--- server/apps/server/src/pay/pay.controller.ts
+++ server/apps/server/src/pay/pay.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Req, All } from '@nestjs/common';
 import type { CreatePayDto } from "@en/common/pay"
 import { PayService } from './pay.service';
 import { AuthGuard } from "@libs/shared/auth/auth.guard"
 import type { Request } from "express"
+
 @Controller('pay')
 export class PayController {
   constructor(private readonly payService: PayService) { }
@@ -10,6 +11,11 @@ export class PayController {
   @UseGuards(AuthGuard)
   @Post()
   create(@Body() createPayDto: CreatePayDto, @Req() req: Request) {
-    return this.payService.create(createPayDto);
+    return this.payService.create(createPayDto, req.user);
+  }
+
+  @All("notify")
+  notify(@Req() req: Request) {
+    return this.payService.notify(req);
   }
 }
--- server/apps/server/src/pay/pay.service.ts
+++ server/apps/server/src/pay/pay.service.ts
@@ -1,10 +1,60 @@
 import { Injectable } from '@nestjs/common';
-import { CreatePayDto } from './dto/create-pay.dto';
-import { UpdatePayDto } from './dto/update-pay.dto';
-
+import type { CreatePayDto } from "@en/common/pay"
+import type { TokenPayload } from '@en/common/user';
+import * as nanoid from "nanoid";
+import dayjs from "dayjs";
+import { ConfigService } from '@nestjs/config';
+import { PrismaService, PayService as SharedPayService } from "@libs/shared"
+import type { Request } from "express"
 @Injectable()
 export class PayService {
-  create(createPayDto: CreatePayDto) {
-    return 'This action adds a new pay';
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+    private readonly sharedPayService: SharedPayService) {
+  }
+
+  private createOutTradeNo() {
+    const prefix = 'EN'
+    return `${prefix}${nanoid.nanoid(12)}`;
+  }
+
+  async create(createPayDto: CreatePayDto, user: TokenPayload) {
+    await this.prisma.$transaction(async (tx) => {
+      // 1. create order
+      const outTradeNo = this.createOutTradeNo();
+      await tx.paymentRecord.create({
+        data: {
+          userId: user.userId,
+          outTradeNo,
+          amount: createPayDto.total_amount,
+          subject: createPayDto.subject,
+          body: createPayDto.body,
+        }
+      })
+      // 2. create sdk
+      const alipaySdk = this.sharedPayService.getAlipaySdk();
+      const payUrl = alipaySdk.pageExecute("alipay.trade.page.pay", "GET", {
+        bizContent: {
+          out_trade_no: outTradeNo,
+          total_amount: createPayDto.total_amount,
+          subject: createPayDto.subject,
+          product_code: "FAST_INSTANT_TRADE_PAY",
+          time_expire: dayjs().add(1, "minute").format("YYYY-MM-DD HH:mm:ss"),
+          body: JSON.stringify({
+            courseId: createPayDto.courseId,
+            userId: user.userId,
+          }),
+          notify_url: `${this.configService.get<string>('ALIPAY_NOTIFY_URL')!}/api/v1/pay/notify`,
+        },
+      });
+      return {
+        payUrl
+      }
+    })
+  }
+
+  notify(req: Request) {
+    return true;
   }
 }
